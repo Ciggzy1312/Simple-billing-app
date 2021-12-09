25c13668d8c62b8d93fa6c1756898f80a14be1fd
Deepayan <deepayanmukherjee1312@gmail.com>
2021-12-10 00:21:04 +0530

    Added user input function for items and tip

--- bill.go
+++ bill.go
@@ -12,7 +12,7 @@ type bill struct {
 func newBill(name string) bill {
 	bill := bill{
 		name:  name,
-		items: map[string]float64{"pie": 4.66, "coffee": 2.5, "soup": 3.2},
+		items: map[string]float64{},
 		tip:   0,
 	}
 
@@ -28,7 +28,17 @@ func (b bill) format() string {
 		total += v
 	}
 
-	fs += fmt.Sprintf("%-15v  $%0.2f", "total:", total)
+	fs += fmt.Sprintf("%-15v  $%d\n", "tip:", b.tip)
+
+	fs += fmt.Sprintf("%-15v  $%0.2f", "total:", total+float64(b.tip))
 
 	return fs
+}
+
+func (b *bill) updateTip(tip int) {
+	b.tip = tip
+}
+
+func (b *bill) addItems(name string, price float64) {
+	b.items[name] = price
 }
--- main.go
+++ main.go
@@ -1,11 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func getInput(prompt string, r *bufio.Reader) (string, error) {
+	fmt.Print(prompt)
+
+	input, err := r.ReadString('\n')
+
+	return strings.TrimSpace(input), err
+}
+
+func promptOptions(b bill) {
+	reader := bufio.NewReader(os.Stdin)
+
+	option, _ := getInput("Choose an option ->\n a -add item\n t - tip bill\n s - save bill\n ", reader)
+
+	switch option {
+	case "a":
+		item, _ := getInput("Enter name of item : ", reader)
+
+		price, _ := getInput("Enter price : ", reader)
+
+		fmt.Println(item, price)
+
+		break
+
+	case "t":
+		tip, _ := getInput("Enter tip : ", reader)
+
+		fmt.Println(tip)
+
+	case "s":
+		fmt.Println("You chose s")
+		break
+
+	default:
+		fmt.Println("Choose a valid option...")
+	}
+
+}
+
+func createBill() bill {
+	reader := bufio.NewReader(os.Stdin)
+
+	/*fmt.Print("Create a new bill name : ")
+
+	name, _ := reader.ReadString('\n')
+	name = strings.TrimSpace(name)*/
+
+	name, _ := getInput("Create a new bill name : ", reader)
+
+	b := newBill(name)
+
+	return b
+}
 
 func main() {
-	mybill := newBill("Mario")
+	mybill := createBill()
+
+	promptOptions(mybill)
+
+	mybill.addItems("crab meat soup", 3.2)
+	mybill.addItems("shepherd's pie", 4.66)
+	mybill.addItems("tiramisu", 2.5)
+
+	mybill.updateTip(5)
 
 	fmt.Println(mybill.format())
-
-	fmt.Println(mybill)
 }
